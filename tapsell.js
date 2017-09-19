c0f87d9d8c8283b48db740518683099e4ef00d4f
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2017-09-19 10:47:59 +0430

    add native ad support

--- tapsell.js
+++ tapsell.js
@@ -1,5 +1,9 @@
 let Tapsell = require("react-native").NativeModules.TapsellReactNative;
 import { DeviceEventEmitter } from "react-native";
+import {
+	onNativeBannerAdShown,
+	onNativeBannerAdClicked
+} from "./tapsell-native";
 
 const ON_AD_AVAILABLE_EVENT = Tapsell.ON_AD_AVAILABLE_EVENT;
 const ON_ERROR_EVENT = Tapsell.ON_ERROR_EVENT;
@@ -8,6 +12,7 @@ const ON_NO_NETWORK_EVENT = Tapsell.ON_NO_NETWORK_EVENT;
 const ON_EXPIRING_EVENT = Tapsell.ON_EXPIRING_EVENT;
 const ON_OPENED_EVENT = Tapsell.ON_OPENED_EVENT;
 const ON_CLOSED_EVENT = Tapsell.ON_CLOSED_EVENT;
+
 const ROTATION_LOCKED_PORTRAIT = Tapsell.ROTATION_LOCKED_PORTRAIT;
 const ROTATION_LOCKED_LANDSCAPE = Tapsell.ROTATION_LOCKED_LANDSCAPE;
 const ROTATION_UNLOCKED = Tapsell.ROTATION_UNLOCKED;
@@ -15,9 +20,15 @@ const ROTATION_LOCKED_REVERSED_PORTRAIT =
 	Tapsell.ROTATION_LOCKED_REVERSED_PORTRAIT;
 const ROTATION_LOCKED_REVERSED_LANDSCAPE =
 	Tapsell.ROTATION_LOCKED_REVERSED_LANDSCAPE;
+
 const PERMISSION_HANDLER_DISABLED = Tapsell.PERMISSION_HANDLER_DISABLED;
 const PERMISSION_HANDLER_AUTO = Tapsell.PERMISSION_HANDLER_AUTO;
 const PERMISSION_HANDLER_AUTO_INSIST = Tapsell.PERMISSION_HANDLER_AUTO_INSIST;
+
+const ON_AD_AVAILABLE_NATIVE_EVENT = Tapsell.ON_AD_AVAILABLE_NATIVE_EVENT;
+const ON_ERROR_NATIVE_EVENT = Tapsell.ON_ERROR_NATIVE_EVENT;
+const ON_NO_AD_AVAILABLE_NATIVE_EVENT = Tapsell.ON_NO_AD_AVAILABLE_NATIVE_EVENT;
+const ON_NO_NETWORK_NATIVE_EVENT = Tapsell.ON_NO_NETWORK_NATIVE_EVENT;
 
 let callbacks = {};
 callbacks.ON_AD_AVAILABLE_EVENT = {};
@@ -28,6 +39,7 @@ callbacks.ON_EXPIRING_EVENT = {};
 callbacks.ON_OPENED_EVENT = {};
 callbacks.ON_CLOSED_EVENT = {};
 
+// Direct Ad Events
 DeviceEventEmitter.addListener(ON_AD_AVAILABLE_EVENT, event => {
 	if (callbacks.ON_AD_AVAILABLE_EVENT[event.zone_id])
 		callbacks.ON_AD_AVAILABLE_EVENT[event.zone_id](
@@ -61,6 +73,32 @@ DeviceEventEmitter.addListener(ON_OPENED_EVENT, event => {
 DeviceEventEmitter.addListener(ON_CLOSED_EVENT, event => {
 	if (callbacks.ON_CLOSED_EVENT[event.ad_id])
 		callbacks.ON_CLOSED_EVENT[event.ad_id](event.zone_id, event.ad_id);
+});
+
+// Native Ad Events
+DeviceEventEmitter.addListener(ON_AD_AVAILABLE_NATIVE_EVENT, event => {
+	if (callbacks.ON_AD_AVAILABLE_NATIVE_EVENT[event.zone_id]) {
+		callbacks.ON_AD_AVAILABLE_NATIVE_EVENT[event.zone_id](
+			event,
+			onNativeBannerAdShown,
+			onNativeBannerAdClicked
+		);
+	}
+});
+DeviceEventEmitter.addListener(ON_ERROR_NATIVE_EVENT, event => {
+	if (callbacks.ON_ERROR_NATIVE_EVENT[event.zone_id]) {
+		callbacks.ON_ERROR_NATIVE_EVENT[event.zone_id](event.error_message);
+	}
+});
+DeviceEventEmitter.addListener(ON_NO_AD_AVAILABLE_NATIVE_EVENT, event => {
+	if (callbacks.ON_NO_AD_AVAILABLE_NATIVE_EVENT[event.zone_id]) {
+		callbacks.ON_NO_AD_AVAILABLE_NATIVE_EVENT[event.zone_id]();
+	}
+});
+DeviceEventEmitter.addListener(ON_NO_NETWORK_NATIVE_EVENT, event => {
+	if (callbacks.ON_NO_NETWORK_NATIVE_EVENT[event.zone_id]) {
+		callbacks.ON_NO_NETWORK_NATIVE_EVENT[event.zone_id]();
+	}
 });
 
 module.exports = {
@@ -110,6 +148,19 @@ module.exports = {
 		callbacks.ON_ERROR_EVENT[zoneId] = onError;
 		callbacks.ON_EXPIRING_EVENT[zoneId] = onExpiring;
 		Tapsell.requestAd(zoneId, isCached);
+	},
+	requestNativeAd: function(
+		zoneId,
+		onAdAvailable,
+		onNoAdAvailable,
+		onNoNetwork,
+		onError
+	) {
+		callbacks.ON_AD_AVAILABLE_NATIVE_EVENT[zoneId] = onAdAvailable;
+		callbacks.ON_NO_AD_AVAILABLE_NATIVE_EVENT[zoneId] = onNoAdAvailable;
+		callbacks.ON_NO_NETWORK_NATIVE_EVENT[zoneId] = onNoNetwork;
+		callbacks.ON_ERROR_NATIVE_EVENT[zoneId] = onError;
+		Tapsell.requestNativeBannerAd(zoneId);
 	},
 	setRewardListener: function(onAdShowFinished) {
 		Tapsell.setRewardListener(onAdShowFinished);
