40770a28ea224bb2ec8b72baa95d96d63bbc9faa
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2017-09-23 14:55:12 +0330

    add aspectRatio for adVideo

--- view/AdVideoComponent.js
+++ view/AdVideoComponent.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import { NativeModules, requireNativeComponent, View } from "react-native";
 import PropTypes from "prop-types";
 
+const aspectRatio = 0.5625;
+
 class AdVideoComponent extends Component {
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
-		return <AdVideo {...this.props} />;
+		return (
+			<AdVideo
+				style={{
+					aspectRatio: 1 / aspectRatio,
+					flex: 1
+				}}
+				{...this.props}
+			/>
+		);
 	}
 }
 
