55d841ea34cc0bcd4020046aca6b11ca45884918
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2017-09-25 14:43:41 +0330

    add setAdId command call onreceiveprops

--- view/AdVideoComponent.js
+++ view/AdVideoComponent.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { NativeModules, requireNativeComponent, View } from "react-native";
+import {
+	NativeModules,
+	requireNativeComponent,
+	View,
+	UIManager,
+	findNodeHandle
+} from "react-native";
 import PropTypes from "prop-types";
 
 const aspectRatio = 0.5625;
@@ -7,6 +13,20 @@ const aspectRatio = 0.5625;
 class AdVideoComponent extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			adId: this.props.adId
+		};
+	}
+
+	componentWillReceiveProps() {
+		if (this.props.adId !== this.state.adId) {
+			this.setState({ adId: this.props.adId });
+			UIManager.dispatchViewManagerCommand(
+				findNodeHandle(this),
+				UIManager.AdVideo.Commands.setAdId,
+				[this.props.adId]
+			);
+		}
 	}
 
 	render() {
