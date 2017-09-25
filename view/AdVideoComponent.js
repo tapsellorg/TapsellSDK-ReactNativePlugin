942e766ec21f4b3c7011c6f540c5ee7a096866b2
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2017-09-25 18:14:36 +0330

    remove auto start and fullscreen for debuging

--- view/AdVideoComponent.js
+++ view/AdVideoComponent.js
@@ -24,11 +24,7 @@ class AdVideoComponent extends Component {
 			UIManager.dispatchViewManagerCommand(
 				findNodeHandle(this),
 				UIManager.AdVideo.Commands.setAdId,
-				[
-					this.props.adId,
-					this.props.autoStartVideo,
-					this.props.fullScreenOnClick
-				]
+				[this.props.adId]
 			);
 		}
 	}
@@ -48,8 +44,6 @@ class AdVideoComponent extends Component {
 
 AdVideoComponent.propTypes = {
 	adId: PropTypes.string.isRequired,
-	autoStartVideo: PropTypes.bool,
-	fullScreenOnClick: PropTypes.bool,
 	...View.propTypes
 };
 
